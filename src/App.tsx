ddf7fac8b01d2bec8b220d410125fe974148bca6
jhnnsrs <jhnnsrs@gmail.com>
2023-03-13 11:36:49 +0100

    Info

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { gql } from "@apollo/client";
 import { Callback, EasyGuard, EasyProvider, useApp } from "@jhnnsrs/arkitekt";
-import { MultiWell } from "./components/MultiWell";
+import { useDatalayer } from "@jhnnsrs/datalayer";
+import { useMikroQuery } from "@jhnnsrs/mikro";
+import { TwoDKanvas, UnkoverProvider } from "@jhnnsrs/unkover";
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { UnkoverProvider } from "@jhnnsrs/unkover";
+import "./App.css";
+
+export const Image = ({ rep }: { rep: any }) => {
+  const { s3resolve } = useDatalayer();
+
+  return (
+    <div className="rounded rounded-md overflow-hidden text-black flex flex-col">
+      <TwoDKanvas path={s3resolve(rep.store)} shape={rep.shape} />
+    </div>
+  );
+};
+
+export const ImageGrid = () => {
+  const { data } = useMikroQuery(gql`
+    query {
+      myrepresentations(limit: 3) {
+        id
+        name
+        shape
+        store
+      }
+    }
+  `);
+
+  return (
+    <div className="grid grid-cols-3 gap-2 mt-1 ">
+      {data?.representations?.map((r: any, index: number) => (
+        <Image rep={r} key={index} />
+      ))}
+    </div>
+  );
+};
 
 export const Test = () => {
   const { manifest } = useApp();
@@ -16,14 +48,20 @@ export const Test = () => {
       </h1>
       <div className="font-light">Lets look at your latest images</div>
       <EasyGuard>
-        <MultiWell />
+        <ImageGrid />
       </EasyGuard>
     </>
   );
 };
 
 function App() {
-  const [count, setCount] = useState(0);
+  // This demo app uses the arkitekt easy provider
+  // which enables the app manifest, configuration
+  // retrieval anf authentication, as well as the
+  // mikro service automatically.
+
+  // For more information on the easy provider
+  // see
 
   return (
     <div className="App">
